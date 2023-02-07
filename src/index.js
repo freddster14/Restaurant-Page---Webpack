0732f06460d3d9237dc7a8e4f2a9ecb747be099c
Zeronicc <zeronic4465@gmail.com>
2023-02-07 03:12:00 -0600

    Made home page mobile friendly & added sidebar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import homeScreen from './home.js'
-import menu from './menu.js'
+import menuScreen from './menu.js'
 import './index.css'
-
 import logo from './logo.png'
+import sidebarLogo from './sidebar.png'
 
 const body = document.querySelector("#content");
 const homeBtn = document.createElement('button');
 const menuBtn = document.createElement('button');
 const contactBtn = document.createElement('button');
+const overlay = document.createElement('div')
 
 function header(){
     const topBar = document.createElement('div');
@@ -27,7 +28,8 @@ function header(){
     containerBtn.id = "container-btn";
     homeBtn.id = "home-btn";
     menuBtn.id = "menu-btn";
-    title.id = "title";
+    title.id = "logo";
+    overlay.id = "overlay";
 
     header.appendChild(topBar);
     topBar.appendChild(title);
@@ -38,8 +40,36 @@ function header(){
     containerBtn.appendChild(contactBtn);
 
     body.appendChild(header);
+    
+
+    let screenWidth  = window.innerWidth || document.documentElement.clientWidth || 
+    document.body.clientWidth;
+    if(screenWidth < 761 )runFunction();
+    
+    function runFunction(){
+        const sidebarBtn = document.createElement('input');
+        sidebarBtn.type = "image";
+        sidebarBtn.src = sidebarLogo;
+        sidebarBtn.id = "sidebar-btn";
+        topBar.appendChild(sidebarBtn);
+        sidebarBtn.addEventListener('click', () => {
+        containerBtn.classList.toggle("active");
+        overlay.classList.toggle("active");
+        });
+        return true
+    }
+    
+    let mq = window.matchMedia("(max-width: 760px)");
+    
+    mq.addListener((event) => {
+      if (event.matches) {
+        runFunction();
+      }else {topBar.removeChild(topBar.lastChild)}
+    })
 
 }
+
+
 
 function removeContent(){
     body.removeChild(body.lastChild)
@@ -59,3 +89,6 @@ homeBtn.addEventListener('click', () => showHome())
 
 header();
 showHome();
+
+body.appendChild(overlay);
+
